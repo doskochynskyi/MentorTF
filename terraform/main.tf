74b4f383706569e21514fa6f4955a4a13e28ce78
Ivan <ivan.doskochynskyi@gmail.com>
2022-06-23 22:54:52 +0300

    add main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,23 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "RGContainer"
+    storage_account_name = "tfstatedevops"
+    container_name       = "contmentortfstate"
+    key                  = "contmentortfstate.tfstate"
+  }
+}
+ 
+provider "azurerm" {
+  # The "feature" block is required for AzureRM provider 2.x.
+  # If you're using version 1.x, the "features" block is not allowed.
+  version = "~>2.0"
+  features {}
+}
+ 
+data "azurerm_client_config" "current" {}
+ 
+#Create Resource Group
+resource "azurerm_resource_group" "RGTF" {
+  name     = "tamops"
+  location = "eastus2"
+}
