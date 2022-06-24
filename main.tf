c85a5ce95f269b4cba9959e70b4546a3d031625d
Ivan <73355620+doskochynskyi@users.noreply.github.com>
2022-06-24 09:39:17 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -27,6 +27,13 @@ resource "azurerm_virtual_network" "net" {
   resource_group_name = data.azurerm_resource_group.rg.name
 }
 
+resource "azurerm_virtual_network" "net" {
+  name                = "tfvnetsite"
+  address_space       = ["192.168.222.0/24"]
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
+}
+
 resource "azurerm_subnet" "net" {
   name                 = "AzureFirewallSubnet"
   resource_group_name  = data.azurerm_resource_group.rg.name
@@ -34,12 +41,12 @@ resource "azurerm_subnet" "net" {
   address_prefixes     = ["192.168.140.0/24"]
 }
 
-resource "azurerm_subnet" "net" {
-  name                 = "aci-subnet"
-  resource_group_name  = data.azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.net.name
-  address_prefixes     = ["192.168.141.0/24"]
-}
+#resource "azurerm_subnet" "net" {
+#  name                 = "aci-subnet"
+#  resource_group_name  = data.azurerm_resource_group.rg.name
+#  virtual_network_name = azurerm_virtual_network.net.name
+#  address_prefixes     = ["192.168.141.0/24"]
+#}
 
 
 
