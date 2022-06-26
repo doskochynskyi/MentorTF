220f32fc05b39f9c91c698d669725d8e24b79230
Ivan <73355620+doskochynskyi@users.noreply.github.com>
2022-06-27 00:06:18 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,13 +5,19 @@ terraform {
     container_name       = "contmentortfstate"
     key                  = "contmentortfstate.tfstate"
   }
+  
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">=3.0.0"
+    }
+  }
 }
 
 
 provider "azurerm" {
   # The "feature" block is required for AzureRM provider 2.x.
-  # If you're using version 1.x, the "features" block is not allowed.
-  version = ">=3.0.0"
+  # If you're using version 1.x, the "features" block is not allowed
   features {}
 }
  
