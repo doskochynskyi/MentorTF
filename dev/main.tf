bca6af82710888f592b981481b81ff3584811256
Ivan <ivan.doskochynskyi@gmail.com>
2022-06-28 23:22:58 +0300

    add net module

--- dev/main.tf
+++ dev/main.tf
@@ -32,7 +32,10 @@ resource "azurerm_resource_group" "rgdev" {
 }
 
 module "network" {
-  source    = "./modules/network"
+  source = "github.com/doskochynskyi/MentorTFModules.git//modules/network/"
+
+  region = var.region
+  resource_group = "RGdev"
 
 }
 
