b6e58173b0eb675a61375d320ce97b5ea983fd0e
Ivan <ivan.doskochynskyi@gmail.com>
2022-06-29 12:49:44 +0300

    add fw module

--- dev/modules.tf
+++ dev/modules.tf
@@ -4,6 +4,13 @@ module "network" {
   region = var.region
   resource_group = "RG${var.prefix}"
 
+}
+
+module "network" {
+  source = "github.com/doskochynskyi/MentorTFModules.git//modules/network"
+
+  region = var.region
+  resource_group = "RG${var.prefix}"
 
 }
 
