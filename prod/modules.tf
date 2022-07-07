212f829033382bdca7d5a7ca38d993bd48f6ddb4
Ivan <73355620+doskochynskyi@users.noreply.github.com>
2022-07-07 21:34:58 +0300

    Update modules.tf

--- prod/modules.tf
+++ prod/modules.tf
@@ -2,6 +2,7 @@ module "network" {
   source = "github.com/doskochynskyi/MentorTFModules.git//modules/network"
 
   region = var.region
+  prefix = "${var.prefix}"
   resource_group = "RG${var.prefix}"
   vnet_address_space = "192.168.0.0/16"
   subnet_firewall_address_space = "192.168.151.0/24"
